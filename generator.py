61c753a2d5f6e63b39c806ad41192736e22b9c3c
Peter Kalverla <peter.kalverla@gmx.com>
2024-10-22 16:07:52 +0200

    Add functions to plot each lcz

--- generator.py
+++ generator.py
@@ -2,6 +2,17 @@ import itertools
 import json
 
 
+def transformation_matrix(translation=(0, 0, 0), scaling=(1, 1, 1)):
+    # fmt: off
+    return [
+        scaling[0], 0, 0, translation[0],
+        0, scaling[1], 0, translation[1],
+        0, 0, scaling[2], translation[2],
+        0, 0, 0, 1,
+    ]
+    # fmt: on
+
+
 class City:
     def __init__(self):
         self.json = {
@@ -74,9 +85,10 @@ class City:
 
         return len(self.json["vertices"]) - 1
 
-    def add_cube(self, x: int, y: int, z: int = 0):
+    def add_template(
+        self, template_id: int, x: int, y: int, z: int = 0, scaling=(1, 1, 1)
+    ) -> str:
         location_id = self.add_vertex(x, y, z)
-        template_id = self.cube_template
         cityobject_id = self.new_cityobject_id()
 
         object = {
@@ -86,24 +98,7 @@ class City:
                     "type": "GeometryInstance",
                     "template": template_id,
                     "boundaries": [location_id],
-                    "transformationMatrix": [
-                        1,
-                        0,
-                        0,
-                        0,
-                        0,
-                        1,
-                        0,
-                        0,
-                        0,
-                        0,
-                        1,
-                        0,
-                        0,
-                        0,
-                        0,
-                        1,
-                    ],
+                    "transformationMatrix": transformation_matrix(scaling=scaling),
                 }
             ],
         }
@@ -111,10 +106,148 @@ class City:
         self.json["CityObjects"][cityobject_id] = object
         return cityobject_id
 
+    def add_cube(self, x: int, y: int, z: int = 0, scaling=(1, 1, 1)) -> str:
+        return self.add_template(self.cube_template, x, y, z, scaling=scaling)
+
+    def add_lcz1(self, x: int, y: int, z: int = 0) -> None:
+        """Add compact high rise on 100m grid cells."""
+        self.add_cube(x - 50, y - 50, scaling=(22.2, 22.2, 37.5))
+        self.add_cube(x - 10, y - 50, scaling=(22.2, 22.2, 37.5))
+        self.add_cube(x + 30, y - 50, scaling=(22.2, 22.2, 37.5))
+
+        self.add_cube(x - 50, y - 10, scaling=(22.2, 22.2, 37.5))
+        self.add_cube(x - 10, y - 10, scaling=(22.2, 22.2, 37.5))
+        self.add_cube(x + 30, y - 10, scaling=(22.2, 22.2, 37.5))
+
+        self.add_cube(x - 50, y + 30, scaling=(22.2, 22.2, 37.5))
+        self.add_cube(x - 10, y + 30, scaling=(22.2, 22.2, 37.5))
+        self.add_cube(x + 30, y + 30, scaling=(22.2, 22.2, 37.5))
+
+    def add_lcz2(self, x: int, y: int, z: int = 0) -> None:
+        """Add compact mid rise on 100m grid cells."""
+        self.add_cube(x - 50, y - 50, scaling=(22, 22, 14))
+        self.add_cube(x - 10, y - 50, scaling=(22, 22, 14))
+        self.add_cube(x + 30, y - 50, scaling=(22, 22, 14))
+
+        self.add_cube(x - 50, y - 10, scaling=(22, 22, 14))
+        self.add_cube(x - 10, y - 10, scaling=(22, 22, 14))
+        self.add_cube(x + 30, y - 10, scaling=(22, 22, 14))
+
+        self.add_cube(x - 50, y + 30, scaling=(22, 22, 14))
+        self.add_cube(x - 10, y + 30, scaling=(22, 22, 14))
+        self.add_cube(x + 30, y + 30, scaling=(22, 22, 14))
+
+    def add_lcz3(self, x: int, y: int, z: int = 0) -> None:
+        """Add compact low rise on 100m grid cells."""
+        for dx, dy in itertools.product([-50, -35, -20, -5, 10, 25, 40], repeat=2):
+            self.add_cube(x + dx, y + dy, scaling=(9.6, 9.6, 6.5))
+
+    def add_lcz4(self, x: int, y: int, z: int = 0) -> None:
+        """Add open high-rise on 100m grid cells."""
+        self.add_cube(x - 50, y, scaling=(42.86, 42.86, 37.5))
+        self.add_cube(x, y - 50, scaling=(42.86, 42.86, 37.5))
+
+    def add_lcz5(self, x: int, y: int, z: int = 0) -> None:
+        self.add_cube(x - 50, y - 50, scaling=(26.25, 26.25, 17.5))
+        self.add_cube(x + 25, y - 50, scaling=(26.25, 26.25, 17.5))
+        self.add_cube(x - 13, y - 13, scaling=(26.25, 26.25, 17.5))
+        self.add_cube(x - 50, y + 25, scaling=(26.25, 26.25, 17.5))
+        self.add_cube(x + 25, y + 25, scaling=(26.25, 26.25, 17.5))
+
+    def add_lcz6(self, x: int, y: int, z: int = 0) -> None:
+        self.add_cube(x - 50, y - 50, scaling=(13, 13, 6.5))
+        self.add_cube(x - 25, y - 50, scaling=(13, 13, 6.5))
+        self.add_cube(x, y - 50, scaling=(13, 13, 6.5))
+        self.add_cube(x + 25, y - 50, scaling=(13, 13, 6.5))
+
+        self.add_cube(x - 37, y - 37, scaling=(13, 13, 6.5))
+        self.add_cube(x - 13, y - 37, scaling=(13, 13, 6.5))
+        self.add_cube(x + 13, y - 37, scaling=(13, 13, 6.5))
+        self.add_cube(x + 37, y - 37, scaling=(13, 13, 6.5))
+
+        self.add_cube(x - 50, y - 25, scaling=(13, 13, 6.5))
+        self.add_cube(x - 25, y - 25, scaling=(13, 13, 6.5))
+        self.add_cube(x, y - 25, scaling=(13, 13, 6.5))
+        self.add_cube(x + 25, y - 25, scaling=(13, 13, 6.5))
+
+        self.add_cube(x - 37, y - 13, scaling=(13, 13, 6.5))
+        self.add_cube(x - 13, y - 13, scaling=(13, 13, 6.5))
+        self.add_cube(x + 13, y - 13, scaling=(13, 13, 6.5))
+        self.add_cube(x + 37, y - 13, scaling=(13, 13, 6.5))
+
+        self.add_cube(x - 50, y, scaling=(13, 13, 6.5))
+        self.add_cube(x - 25, y, scaling=(13, 13, 6.5))
+        self.add_cube(x, y, scaling=(13, 13, 6.5))
+        self.add_cube(x + 25, y, scaling=(13, 13, 6.5))
+
+        self.add_cube(x - 37, y + 13, scaling=(13, 13, 6.5))
+        self.add_cube(x - 13, y + 13, scaling=(13, 13, 6.5))
+        self.add_cube(x + 13, y + 13, scaling=(13, 13, 6.5))
+        self.add_cube(x + 37, y + 13, scaling=(13, 13, 6.5))
+
+        self.add_cube(x - 50, y + 25, scaling=(13, 13, 6.5))
+        self.add_cube(x - 25, y + 25, scaling=(13, 13, 6.5))
+        self.add_cube(x, y + 25, scaling=(13, 13, 6.5))
+        self.add_cube(x + 25, y + 25, scaling=(13, 13, 6.5))
+
+        self.add_cube(x - 37, y + 37, scaling=(13, 13, 6.5))
+        self.add_cube(x - 13, y + 37, scaling=(13, 13, 6.5))
+        self.add_cube(x + 13, y + 37, scaling=(13, 13, 6.5))
+        self.add_cube(x + 37, y + 37, scaling=(13, 13, 6.5))
+
+    def add_lcz7(self, x: int, y: int, z: int = 0) -> None:
+        self.add_cube(x - 50, y - 50, scaling=(25, 25, 3))
+        self.add_cube(x - 22, y - 50, scaling=(25, 25, 3))
+        self.add_cube(x + 6, y - 50, scaling=(25, 25, 3))
+        self.add_cube(x + 34, y - 50, scaling=(15, 25, 3))
+
+        self.add_cube(x - 50, y - 22, scaling=(15, 25, 3))
+        self.add_cube(x - 32, y - 22, scaling=(25, 25, 3))
+        self.add_cube(x - 4, y - 22, scaling=(25, 25, 3))
+        self.add_cube(x + 24, y - 22, scaling=(25, 25, 3))
+
+        self.add_cube(x - 50, y + 6, scaling=(25, 25, 3))
+        self.add_cube(x - 22, y + 6, scaling=(25, 25, 3))
+        self.add_cube(x + 6, y + 6, scaling=(25, 25, 3))
+        self.add_cube(x + 34, y + 6, scaling=(15, 25, 3))
+
+        self.add_cube(x - 50, y + 34, scaling=(15, 15, 3))
+        self.add_cube(x - 32, y + 34, scaling=(25, 15, 3))
+        self.add_cube(x - 4, y + 34, scaling=(25, 15, 3))
+        self.add_cube(x + 24, y + 34, scaling=(25, 15, 3))
+
+    def add_lcz8(self, x: int, y: int, z: int = 0) -> None:
+        self.add_cube(x - 45, y - 45, scaling=(28.9, 28.9, 6.5))
+        self.add_cube(x + 15, y - 45, scaling=(28.9, 28.9, 6.5))
+        self.add_cube(x - 15, y - 15, scaling=(28.9, 28.9, 6.5))
+        self.add_cube(x - 45, y + 15, scaling=(28.9, 28.9, 6.5))
+        self.add_cube(x + 15, y + 15, scaling=(28.9, 28.9, 6.5))
+
+    def add_lcz9(self, x: int, y: int, z: int = 0) -> None:
+        """Sparsely built."""
+        self.add_cube(x - 45, y - 45, scaling=(43.33, 43.33, 6.5))
+        self.add_cube(x + 5, y + 5, scaling=(43.33, 43.33, 6.5))
+
+    def add_lcz10(self, x: int, y: int, z: int = 0) -> None:
+        """Heavy industry."""
+        self.add_cube(x - 50, y - 50, scaling=(23.8, 23.8, 10))
+        self.add_cube(x, y, scaling=(23.8, 23.8, 10))
+
 
 if __name__ == "__main__":
     city = City()
-    for x, y in zip([0, 0], [2, 0]):
-        city.add_cube(x, y)
+    # for x, y in zip([0, 0], [2, 0]):
+    #     city.add_cube(x, y)
 
+    city.add_lcz1(50, 50)
+    city.add_lcz2(50, 150)
+    city.add_lcz3(50, 250)
+    city.add_lcz4(150, 50)
+    city.add_lcz5(150, 150)
+    city.add_lcz6(150, 250)
+    city.add_lcz7(250, 50)
+    city.add_lcz8(50, 350)
+    city.add_lcz9(150, 350)
+    city.add_lcz10(50, 450)
     city.to_cityjson("test.city.json")
+    print("Done")
---
+++ lczs.md
@@ -0,0 +1,17 @@
+# LCZs in WRF
+
+Relevant parameters from the URBPARM_LCZ.TBL for each of the local climate zones:
+
+| LCZ     | Name                 | Roof level | Std roof height | Roof width | Road width | FRC_URB |
+| ------- | -------------------- | ---------- | --------------- | ---------- | ---------- | ------- |
+| lcz1    | compact high-rise    | 37.5       | 4.0             | 22.2       | 20.        | 0.95    |
+| lcz2    | compact mid-rise     | 17.5       | 3.0             | 22.        | 14.        | 0.9     |
+| lcz3    | compact low-rise     | 6.5        | 1.0             | 9.6        | 5.2        | 0.85    |
+| lcz4    | open high-rise       | 37.5       | 1.              | 42.86      | 50.0       | 0.65    |
+| lcz5    | open mid-rise        | 17.5       | 1.              | 26.25      | 35.0       | 0.7     |
+| lcz6    | open low-rise        | 6.5        | 1.              | 13.        | 13.0       | 0.6     |
+| lcz7    | lightweight low-rise | 3.         | 1.              | 25.        | 3.33       | 0.85    |
+| lcz8    | large low-rise       | 6.5        | 1.              | 28.9       | 32.5       | 0.85    |
+| lcz9    | sparsely built       | 6.5        | 1.              | 43.33      | 43.3       | 0.3     |
+| lcz10   | heavy industry       | 10.        | 1.              | 23.8       | 28.6       | 0.55    |
+| (lcz11) | asphalt              | 10.        | 1.              | 5.         | 100.0      | 1.00    |
